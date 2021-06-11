00023502f78498ad08790c64179a875b5ec1ed81
jmikk <buldog.buldog26@gmail.com>
2021-06-10 20:28:46 -0500

    Update No Region Move.js

--- No Region Move.js
+++ No Region Move.js
@@ -1,20 +1,18 @@
 // ==UserScript==
-// @name         No region move
+// @name         Anti Region move
 // @version      0.1
-// @namespace    dithpri
-// @description  prevent moving regions on main
-// @author       dithpri stole with permsion by 9003
-// @noframes
+// @description  Remove the move region button
+// @author       9003 (Thanks dithpri for the oringal code)
 // @match        https://www.nationstates.net/*region=*
+// @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
     if (document.body.dataset.nname == '9003') {
-        document
-            .querySelectorAll('button[name="move_region"]')
-            .forEach(function (el, idx) {
-            el.disabled = true;
-        });
+
+    var butts = document.getElementsByName("button danger icon ");
+       butts[1].remove;
     }
+    // Your code here...
 })();
